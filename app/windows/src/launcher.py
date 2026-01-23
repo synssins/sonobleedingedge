1abf63be4b528c03501158c1616762e1846c60e3
Synssins <synssins@gmail.com>
2026-01-22 19:29:10 -0600

    chore: bump version to 0.0.48 for Windows build test

--- app/windows/src/launcher.py
+++ app/windows/src/launcher.py
@@ -39,7 +39,7 @@ from PyQt6.QtGui import QIcon, QPixmap, QAction, QDesktopServices, QFont, QTextC
 
 # Constants
 APP_NAME = "Sonorium"
-APP_VERSION = "0.0.47"
+APP_VERSION = "0.0.48"
 DEFAULT_PORT = 8008
 
 # Global logger instance
